36eb75e6d01b64535caeff3ca99a4a6a3f1db415
ello1010@naver.com <ello1010@naver.com>
2023-04-11 13:20:38 +0900

    Editing...

--- Sources/MHAPI/MHAPI.swift
+++ Sources/MHAPI/MHAPI.swift
@@ -13,6 +13,73 @@ public protocol MH_API: AnyObject{
 
 public extension MH_API{
     
+//    func call<T: MH_APIInfo>(api: T, completed: @escaping (T.ResponseType)->()){
+//
+//        self.session.request(URL(string: api.address)!, method: api.method, parameters: api.parameters, headers: api.config?.headers).responseData { res in
+//            switch res.result{
+//            case .success(_):
+//                if let data = res.value{
+//                    do{
+//                        let decodingData = try JSONDecoder().decode(T.ResponseType.self, from: data)
+//                        if let _ = decodingData.data{
+//                            completed(decodingData)
+//                        }else{
+//                            completed(T.ResponseType(responseType: .error(code: -1, message: "decoding error"), data: nil))
+//                        }
+//                    }catch(let e){
+//                        completed(T.ResponseType(responseType: .error(code: e.asAFError?.responseCode ?? -1, message: e.localizedDescription), data: nil))
+//                    }
+//                }else{
+//                    completed(T.ResponseType(responseType: .error(code: res.error?.responseCode ?? -1, message: res.error?.localizedDescription), data: nil))
+//                }
+//                break
+//            case .failure(_):
+//                completed(T.ResponseType(responseType: .error(code: res.error?.responseCode ?? -1, message: res.error?.localizedDescription), data: nil))
+//                break
+//            }
+//        }
+//    }
+//
+//    func callByRx<T: MH_APIInfo, R: Response_P>(_ api: T) -> Observable<R> where T.ResponseType == R {
+//
+//        return Observable<R>.create { observer in
+//            #if DEBUG
+//            print("=======================")
+//            print("📲url: \(api.address)")
+//            print("📲parameters: \(String(describing: api.parameters))")
+//            print("📲method: \(api.method)")
+//            print("📲header: \(String(describing: api.config?.headers))")
+//            #endif
+//            let request = self.session.request(URL(string: api.address)!, method: api.method, parameters: api.parameters, headers: api.config?.headers).responseData { res in
+//                switch res.result{
+//                case .success(_):
+//                    if let data = res.value{
+//                        do{
+//                            let decodingData = try JSONDecoder().decode(R.self, from: data)
+//                            observer.onNext(decodingData)
+//                        }catch let e{
+//                            observer.onNext(R(responseType: ResponseType.error(code: (e as? AFError)?.responseCode ?? -1, message: e.localizedDescription), data: nil))
+//                        }
+//                    }else{
+//                        observer.onNext(R(responseType: ResponseType.error(code: (res.error as? AFError)?.responseCode ?? -1, message: res.error?.localizedDescription), data: nil))
+//                    }
+//                    break
+//                case .failure(_):
+//                    observer.onNext(R(responseType: ResponseType.error(code: (res.error as? AFError)?.responseCode ?? -1, message: res.error?.localizedDescription), data: nil))
+//                    break
+//                }
+//                observer.onCompleted()
+//            }.responseString { res in
+//                #if DEBUG
+//                print("responseString \(String(describing: res.value))")
+//                print("=======================")
+//                #endif
+//            }
+//            return Disposables.create {
+//                request.cancel()
+//            }
+//        }
+//    }
     func call<T: MH_APIInfo>(api: T, completed: @escaping (T.ResponseType)->()){
         
         self.session.request(URL(string: api.address)!, method: api.method, parameters: api.parameters, headers: api.config?.headers).responseData { res in
@@ -37,43 +104,38 @@ public extension MH_API{
                 completed(T.ResponseType(responseType: .error(code: res.error?.responseCode ?? -1, message: res.error?.localizedDescription), data: nil))
                 break
             }
+        }.responseString { res in
+            print("res::\(res)")
         }
     }
     
     func callByRx<T: MH_APIInfo, R: Response_P>(_ api: T) -> Observable<R> where T.ResponseType == R {
         
         return Observable<R>.create { observer in
-            #if DEBUG
-            print("=======================")
-            print("📲url: \(api.address)")
-            print("📲parameters: \(String(describing: api.parameters))")
-            print("📲method: \(api.method)")
-            print("📲header: \(String(describing: api.config?.headers))")
-            #endif
+            
             let request = self.session.request(URL(string: api.address)!, method: api.method, parameters: api.parameters, headers: api.config?.headers).responseData { res in
                 switch res.result{
                 case .success(_):
                     if let data = res.value{
                         do{
                             let decodingData = try JSONDecoder().decode(R.self, from: data)
-                            observer.onNext(decodingData)
-                        }catch let e{
-                            observer.onNext(R(responseType: ResponseType.error(code: (e as? AFError)?.responseCode ?? -1, message: e.localizedDescription), data: nil))
+//                            if let _ = decodingData.data{
+                                observer.onNext(decodingData)
+//                            }else{
+//                                observer.onError(APICallError.decodingErr(code: -1, message: "decoding error"))
+//                            }
+                        }catch(let e){
+                            observer.onError(APICallError.decodingErr(code: e.asAFError?.responseCode ?? -1, message: e.localizedDescription))
                         }
                     }else{
-                        observer.onNext(R(responseType: ResponseType.error(code: (res.error as? AFError)?.responseCode ?? -1, message: res.error?.localizedDescription), data: nil))
+                        observer.onError(APICallError.noDataErr(code: res.error?.responseCode ?? -1, message: res.error?.localizedDescription))
                     }
                     break
                 case .failure(_):
-                    observer.onNext(R(responseType: ResponseType.error(code: (res.error as? AFError)?.responseCode ?? -1, message: res.error?.localizedDescription), data: nil))
+                    observer.onError(APICallError.networkingErr(code: res.error?.responseCode ?? -1, message: res.error?.localizedDescription))
                     break
                 }
                 observer.onCompleted()
-            }.responseString { res in
-                #if DEBUG
-                print("responseString \(String(describing: res.value))")
-                print("=======================")
-                #endif
             }
             return Disposables.create {
                 request.cancel()
@@ -83,3 +145,35 @@ public extension MH_API{
 }
 
 
+public enum APICallError: Error{
+    case decodingErr(code: Int, message: String?)
+    case noDataErr(code: Int, message: String?)
+    case networkingErr(code: Int, message: String?)
+    case etcErr(code: Int, message: String?)
+    
+    var desc: String?{
+        switch self {
+        case .decodingErr(_, let message):
+            return message
+        case .noDataErr(_, let message):
+            return message
+        case .networkingErr(_, let message):
+            return message
+        case .etcErr(_, let message):
+            return message
+        }
+    }
+    
+    var code: Int{
+        switch self {
+        case .decodingErr(let code, _):
+            return code
+        case .noDataErr(let code, _):
+            return code
+        case .networkingErr(let code, _):
+            return code
+        case .etcErr(let code, _):
+            return code
+        }
+    }
+}
--- Tests/MHAPITests/MHAPITests.swift
+++ Tests/MHAPITests/MHAPITests.swift
@@ -6,6 +6,6 @@ final class MHAPITests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(MHAPI().text, "Hello, World!")
+//        XCTAssertEqual(MHAPI().text, "Hello, World!")
     }
 }
